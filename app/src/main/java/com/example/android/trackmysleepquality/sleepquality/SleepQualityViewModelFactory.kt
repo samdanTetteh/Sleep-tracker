9586eb6fd89b60ddc8ae4279d5f008e83da1dc73
Samdan <samdan@ijikod.com>
2022-06-20 21:29:38 +0100

    implemented navigation and sleep quality recordings

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
@@ -15,3 +15,21 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+
+class SleepQualityViewModelFactory (
+    private val sleepNightKey: Long,
+    private val sleepDataSource: SleepDatabaseDao): ViewModelProvider.Factory {
+
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepQualityViewModel::class.java)) {
+            return SleepQualityViewModel(sleepNightKey, sleepDataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
