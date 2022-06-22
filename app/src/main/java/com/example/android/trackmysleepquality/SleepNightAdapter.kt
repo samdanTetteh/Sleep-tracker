a804837b08e7b7f9be8b01c0c2840928939fd4f8
Samdan Kudjodji <samdantettey@gmail.com>
2022-06-22 05:37:38 +0100

    added viewholder view manipulation

--- app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.android.trackmysleepquality
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -18,6 +19,11 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val dataItem = data[position]
+        if (position <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
         holder.textView.text = dataItem.sleepQuality.toString()
     }
 
