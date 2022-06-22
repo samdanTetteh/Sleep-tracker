72a48db54fef787bdb42cfc1900b7a24b914c517
Samdan Kudjodji <samdantettey@gmail.com>
2022-06-22 06:46:48 +0100

    refactor onCreateViewHolder

--- app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
@@ -21,23 +21,9 @@ class SleepNightAdapter: RecyclerView.Adapter<ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val dataItem = data[position]
-        val res = holder.itemView.context.resources
-
-        dataItem.apply {
-            holder.sleepLength.text = convertDurationToFormatted(startTimeMilli, endTimeMilli, res)
-            holder.quality.text = convertNumericQualityToString(dataItem.sleepQuality, res)
-
-            holder.qualityImage.setImageResource( when (dataItem.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            })
-        }
+        holder.bind(dataItem)
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -52,6 +38,26 @@ class ViewHolder(viewItem: View): RecyclerView.ViewHolder(viewItem) {
     val sleepLength = viewItem.findViewById<TextView>(R.id.sleep_length)
     val quality = viewItem.findViewById<TextView>(R.id.quality_string)
     val qualityImage = viewItem.findViewById<ImageView>(R.id.quality_image)
+    val res = viewItem.context.resources
 
+
+     fun bind(dataItem: SleepNight) {
+        dataItem.apply {
+            sleepLength.text = convertDurationToFormatted(startTimeMilli, endTimeMilli, res)
+            quality.text = convertNumericQualityToString(dataItem.sleepQuality, res)
+
+            qualityImage.setImageResource(
+                when (dataItem.sleepQuality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_active
+                }
+            )
+        }
+    }
 
 }
