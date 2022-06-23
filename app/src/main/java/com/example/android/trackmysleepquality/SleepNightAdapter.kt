9e1c7571ab61e383cac964caf3f276097aac73e1
Samdan Kudjodji <samdantettey@gmail.com>
2022-06-23 15:48:39 +0100

    implemented data binding adapters

--- app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
@@ -38,23 +38,8 @@ class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
 class ViewHolder private constructor(private val binding: ListItemSleepNightBinding): RecyclerView.ViewHolder(binding.root) {
 
      fun bind(dataItem: SleepNight) {
-         val res = itemView.context.resources
-         dataItem.apply {
-            binding.sleepLength.text = convertDurationToFormatted(startTimeMilli, endTimeMilli, res)
-            binding.qualityString.text = convertNumericQualityToString(dataItem.sleepQuality, res)
-
-            binding.qualityImage.setImageResource(
-                when (dataItem.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
-        }
+         binding.sleep = dataItem
+         binding.executePendingBindings()
     }
 
     companion object {
